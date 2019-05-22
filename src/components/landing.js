8d01553a5f170370439bdf03d0b4913ec4586941
daneyul <dan.nguyen5@gmail.com>
2019-05-22 17:21:33 +0900

    added titles

--- src/components/landing.js
+++ src/components/landing.js
@@ -7,7 +7,17 @@ import Img from "gatsby-image"
 
 
 export default ({ data }) => (
-    <div>
-      <h1>Hello gatsby-image</h1>
+    <div class="container">
+      <div class="row justify-content-end">
+        <div class="col-12">
+          <h1 class="landing-title text-right">추석<br />Chuseok</h1>
+          <h2 class="landing-title2 text-right">September 12-14, 2019</h2>
+        </div>
+      </div>
+      <div class="row justify-content-end landing-title3--margin">
+        <div class="col-12">
+          <h3 class="landing-leader text-right"><span class="landing-title3">Celebrate the Korean Harvest Festival and learn about what it is, what is <br class="landing-title3--break"/>eaten, or the elaborate table settings during these special three days.</span></h3>
+        </div>
+      </div>
     </div>
   )
