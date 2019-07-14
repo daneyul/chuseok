4c3e13252700cf21d084234e64cedfecea30e0f4
daneyul <dan.nguyen5@gmail.com>
2019-07-14 21:53:14 +0900

    fixed footer nav

--- src/components/footer.js
+++ src/components/footer.js
@@ -34,8 +34,8 @@ const Footer = () => (
         <div class="col-2 footer-bottom">
             <h5 class="footer-category"><span class="footer-color footer-font landing-category--dotted footer-category">Festivities</span></h5>
             <ul>
-                <li><a href="#places" class="footer-font footer-color footer-link">Places to Visit</a></li>
-                <li><a href="#media" class="footer-font footer-color footer-link">Shows & Media</a></li>
+                <li><a href="/#places" class="footer-font footer-color footer-link">Places to Visit</a></li>
+                <li><a href="/#media" class="footer-font footer-color footer-link">Shows & Media</a></li>
                 <li><a href="/hanbok" onClick="window.location.reload();" class="footer-font footer-color footer-link">Hanbok</a></li>
                 <li><a href="/gifts" onClick="window.location.reload();" class="footer-font footer-color footer-link">Gifts</a></li>
                 <li><a href="/greetings" onClick="window.location.reload();" class="footer-font footer-color footer-link">Greetings</a></li>
