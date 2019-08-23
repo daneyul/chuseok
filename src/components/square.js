4e35c517c4fa477575e0b2b33840d72bb353d195
daneyul <dan.nguyen5@gmail.com>
2019-08-23 21:08:40 +0900

    ugh

--- src/components/square.js
+++ src/components/square.js
@@ -4,14 +4,14 @@ import React from 'react';
 const Ads = () => (
   <React.Fragment>
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <ins class='adsbygoogle'
-      data-ad-client='ca-pub-2932074618261080'
-      data-ad-slot='9229482019'
-      data-ad-format='auto'
-      data-full-width-responsive="true" />
+    <ins class="adsbygoogle"
+        data-ad-client="ca-pub-2932074618261080"
+        data-ad-slot="9229482019"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
     <script>
-      (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>   
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
   </React.Fragment>
 )
 
--- src/components/vertical.js
+++ src/components/vertical.js
@@ -2,16 +2,17 @@ import React from 'react';
 
 const Adv = () => (
   <React.Fragment>
-  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-  <ins className='adsbygoogle'
-    data-ad-client='ca-pub-2932074618261080'
-    data-ad-slot='5332321809'
-    data-ad-format='auto'
-    data-full-width-responsive="true" />
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-2932074618261080"
+        data-ad-slot="5332321809"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
     <script>
-      (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>   
-    </React.Fragment>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+  </React.Fragment>
 )
 
 export default Adv
