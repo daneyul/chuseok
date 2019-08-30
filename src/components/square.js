0d4d6828e5c14c6530ce0a92350b589a73ef0530
daneyul <dan.nguyen5@gmail.com>
2019-08-30 20:21:00 +0900

    friday changes

--- src/components/square.js
+++ src/components/square.js
@@ -9,7 +9,7 @@ export default class Ads extends React.Component {
     return (
       <React.Fragment>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <ins class="adsbygoogle"
+        <ins class="adsbygoogle ads"
             data-ad-client="ca-pub-2932074618261080"
             data-ad-slot="9229482019"
             data-ad-format="auto"
