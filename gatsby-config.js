db0c8e4c716bf77ae7fb4e6479d8c73c88006f32
daniel <dan.nguyen5@gmail.com>
2020-09-01 17:03:02 +0900

    change config

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,16 +20,16 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name:`pages`,
+        name: `pages`,
         path: `${__dirname}/src/pages`,
       },
     },
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          name:`images`,
-          path: `${__dirname}/src/images`,
-        },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -53,8 +53,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#152555`,
+        theme_color: `#152555`,
         display: `minimal-ui`,
         icon: `src/images/chuseok-icon.png`, // This path is relative to the root of the site.
       },
@@ -70,8 +70,8 @@ module.exports = {
 
 
   ]
-    
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+
+  // this (optional) plugin enables Progressive Web App + Offline functionality
+  // To learn more, visit: https://gatsby.dev/offline
+  // `gatsby-plugin-offline`,
 }
