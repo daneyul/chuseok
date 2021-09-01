ce6f147143ace2c0d86a42f819145d7dc96fb8af
Daniel Nguyen <daniel@monograph.com>
2021-09-01 10:26:03 -0700

    add seo component import

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from "../components/nav";
 import Footer from "../components/footer";
+import SEO from '../components/seo';
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/blog.scss';
 import '../styles/landing.scss';
