5598bb0b94d5499f4579fc4112a700471962000d
Daniel Nguyen <daniel@monograph.com>
2021-12-01 11:11:45 -0800

    added title

--- src/components/seo.js
+++ src/components/seo.js
@@ -8,24 +8,26 @@
 import React from "react"
 import Helmet from "react-helmet"
 
-const SEO = () => {
+const year = new Date().getFullYear()
+
+const SEO = ({title}) => {
 
   return (
 
     <Helmet>
-      <title>Chuseok 2021 - Korean Thanksgiving</title>
-      <meta name="title" content="Chuseok 2021 - Korean Thanksgiving" />
+      <title>{`Chuseok ${year} - ${title}`}</title>
+      <meta name="title" content={`Chuseok ${year} - Korean Thanksgiving`} />
       <meta name="description" content="Celebrate and learn about the foods, traditions, and festivities of one of Korea's most important holidays, Chuseok." />
 
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://www.chuseok.info/" />
-      <meta property="og:title" content="Chuseok 2021 - Korean Thanksgiving" />
+      <meta property="og:title" content={`Chuseok ${year} - Korean Thanksgiving`} />
       <meta property="og:description" content="Celebrate and learn about the foods, traditions, and festivities of one of Korea's most important holidays, Chuseok." />
       <meta property="og:image" content="../images/meta.jpg" />
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://www.chuseok.info/" />
-      <meta property="twitter:title" content="Chuseok 2021 - Korean Thanksgiving" />
+      <meta property="twitter:title" content={`Chuseok ${year} - Korean Thanksgiving`} />
       <meta property="twitter:description" content="Celebrate and learn about the foods, traditions, and festivities of one of Korea's most important holidays, Chuseok." />
       <meta property="twitter:image" content="../images/meta.jpg" />
 
