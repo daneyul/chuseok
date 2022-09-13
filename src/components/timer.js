c34c21511286f65d9611d6bcfce3ea3ac03b0e05
Daniel Nguyen <daniel@monograph.com>
2022-09-13 15:47:12 +0900

    change title again

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,12 +1,12 @@
 import React from "react"
 
 const Timer = () => {
-  let countDownDate = new Date("Sep 10, 2022").getTime();
+  let countDownDate = new Date("Sep 29, 2023").getTime();
   let now = new Date().getTime();
   let distance = countDownDate - now;
   let days = Math.floor(distance / (1000 * 60 * 60 * 24));
   return(
-    <h5 class="text-center landing-title4">It's Chuseok weekend!</h5>
+    <h5 class="text-center landing-title4">{days} until next Chuseok!</h5>
   )
 }
 
