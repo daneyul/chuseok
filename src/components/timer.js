deddfccc19eb399293f29062f0009ae2fc127a3d
Daniel Nguyen <daniel@monograph.com>
2023-08-05 13:30:27 +0900

    days

--- src/components/timer.js
+++ src/components/timer.js
@@ -6,7 +6,7 @@ const Timer = () => {
   let distance = countDownDate - now;
   let days = Math.floor(distance / (1000 * 60 * 60 * 24));
   return(
-    <h5 class="text-center landing-title4">{days} days until next Chuseok!</h5>
+    <h5 class="text-center landing-title4">{`${days} days until next Chuseok!`}</h5>
   )
 }
 
